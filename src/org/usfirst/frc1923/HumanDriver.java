b4e588d870d7d24b626aa3c559e925da562f30e3
Aaron Weiss <aaronweiss74@gmail.com>
2012-02-10 00:06:45 -0500

    Reformated all source files.

--- src/org/usfirst/frc1923/HumanDriver.java
+++ src/org/usfirst/frc1923/HumanDriver.java
@@ -11,24 +11,26 @@ public class HumanDriver {
 	private Gearbox driveGearbox;
 	private DriveTrain driveTrain;
 	private Relay bridgeKnockerDowner;
-	
+
 	private boolean wtf = false;
 	private int shooterSpeed = 0;
-			
-	public HumanDriver(DriveTrain driveTrain, Shooter shooter, Components components) {
+
+	public HumanDriver(DriveTrain driveTrain, Shooter shooter,
+			Components components) {
 		this.driveTrain = driveTrain;
 		this.shooter = shooter;
-		driveGearbox =  new Gearbox(Configuration.driveGears, components);
+		driveGearbox = new Gearbox(Configuration.driveGears, components);
 		leftDriveStick = components.leftDriveStick;
 		rightDriveStick = components.rightDriveStick;
 		operatorController = components.operatorController;
 		bridgeKnockerDowner = components.bridgeKnockerDowner;
 		components.drive.setSafetyEnabled(false);
 	}
-	
+
 	public void handleActiveDriving() {
 		if (Configuration.reversedControls) {
-			driveTrain.drive(rightDriveStick.getCoalescedY(), leftDriveStick.getCoalescedY());
+			driveTrain.drive(rightDriveStick.getCoalescedY(),
+					leftDriveStick.getCoalescedY());
 		} else {
 			double left = -leftDriveStick.getCoalescedY();
 			double right = -rightDriveStick.getCoalescedY();
@@ -37,25 +39,31 @@ public class HumanDriver {
 			driveTrain.drive(left, right);
 		}
 	}
-	
+
 	public void handlePassiveDriving() {
 		if (Configuration.gearShifter) {
-			if (leftDriveStick.getButton(Joystick.ButtonType.kTrigger) && !rightDriveStick.getButton(Joystick.ButtonType.kTrigger) && !driveGearbox.didJustGearDown()) {
+			if (leftDriveStick.getButton(Joystick.ButtonType.kTrigger)
+					&& !rightDriveStick.getButton(Joystick.ButtonType.kTrigger)
+					&& !driveGearbox.didJustGearDown()) {
 				driveGearbox.gearDown();
-			} else if (rightDriveStick.getButton(Joystick.ButtonType.kTrigger) && !leftDriveStick.getButton(Joystick.ButtonType.kTrigger) && !driveGearbox.didJustGearUp()) {
+			} else if (rightDriveStick.getButton(Joystick.ButtonType.kTrigger)
+					&& !leftDriveStick.getButton(Joystick.ButtonType.kTrigger)
+					&& !driveGearbox.didJustGearUp()) {
 				driveGearbox.gearUp();
-			} else if (!leftDriveStick.getButton(Joystick.ButtonType.kTrigger) && !rightDriveStick.getButton(Joystick.ButtonType.kTrigger)) {
+			} else if (!leftDriveStick.getButton(Joystick.ButtonType.kTrigger)
+					&& !rightDriveStick.getButton(Joystick.ButtonType.kTrigger)) {
 				driveGearbox.resetGearControls();
 			}
 		} else {
-			if (leftDriveStick.getButton(Joystick.ButtonType.kTrigger) && rightDriveStick.getButton(Joystick.ButtonType.kTrigger)) {
-	            driveGearbox.setGear(6);
-	        } else {
-	        	driveGearbox.setGear(0);
-	        }
+			if (leftDriveStick.getButton(Joystick.ButtonType.kTrigger)
+					&& rightDriveStick.getButton(Joystick.ButtonType.kTrigger)) {
+				driveGearbox.setGear(6);
+			} else {
+				driveGearbox.setGear(0);
+			}
 		}
 	}
-	
+
 	public void handleActiveOperating() {
 		if (rightDriveStick.getRawButton(5)) {
 			shooter.run(Configuration.shooterGears[shooterSpeed]);
@@ -73,13 +81,15 @@ public class HumanDriver {
 		} else {
 			bridgeKnockerDowner.set(Relay.Value.kOff);
 		}
-		if (rightDriveStick.getRawButton(11) && shooterSpeed < Configuration.shooterGears.length) {
+		if (rightDriveStick.getRawButton(11)
+				&& shooterSpeed < Configuration.shooterGears.length) {
 			shooterSpeed++;
 			wtf = true;
 		} else if (rightDriveStick.getRawButton(10) && shooterSpeed > 0) {
 			shooterSpeed--;
 			wtf = true;
-		} else if (!rightDriveStick.getRawButton(11) && !rightDriveStick.getRawButton(10)) {
+		} else if (!rightDriveStick.getRawButton(11)
+				&& !rightDriveStick.getRawButton(10)) {
 			wtf = false;
 		}
 	}
--- src/org/usfirst/frc1923/XboxController.java
+++ src/org/usfirst/frc1923/XboxController.java
@@ -3,66 +3,66 @@ package org.usfirst.frc1923;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class XboxController {
-    private Joystick xboxController;
+	private Joystick xboxController;
 
-    public static class Button {
-        public final int value;
-        public static final int kA_val = 1;
-        public static final int kB_val = 2;
-        public static final int kX_val = 3;
-        public static final int kY_val = 4;
-        public static final int kLB_val = 5;
-        public static final int kRB_val = 6;
-        public static final int kStart_val = 8;
-        public static final int kBack_val = 7;
-        public static final int kLeftClick_val = 9;
-        public static final int kRightClick_val = 10;
-        public static final Button A = new Button(kA_val);
-        public static final Button B = new Button(kB_val);
-        public static final Button X = new Button(kX_val);
-        public static final Button Y = new Button(kY_val);
-        public static final Button LB = new Button(kLB_val);
-        public static final Button RB = new Button(kRB_val);
-        public static final Button Start = new Button(kStart_val);
-        public static final Button Back = new Button(kBack_val);
-        public static final Button LeftClick = new Button(kLeftClick_val);
-        public static final Button RightClick = new Button(kRightClick_val);
+	public static class Button {
+		public final int value;
+		public static final int kA_val = 1;
+		public static final int kB_val = 2;
+		public static final int kX_val = 3;
+		public static final int kY_val = 4;
+		public static final int kLB_val = 5;
+		public static final int kRB_val = 6;
+		public static final int kStart_val = 8;
+		public static final int kBack_val = 7;
+		public static final int kLeftClick_val = 9;
+		public static final int kRightClick_val = 10;
+		public static final Button A = new Button(kA_val);
+		public static final Button B = new Button(kB_val);
+		public static final Button X = new Button(kX_val);
+		public static final Button Y = new Button(kY_val);
+		public static final Button LB = new Button(kLB_val);
+		public static final Button RB = new Button(kRB_val);
+		public static final Button Start = new Button(kStart_val);
+		public static final Button Back = new Button(kBack_val);
+		public static final Button LeftClick = new Button(kLeftClick_val);
+		public static final Button RightClick = new Button(kRightClick_val);
 
-        private Button(int value) {
-            this.value = value;
-        }
-    }
+		private Button(int value) {
+			this.value = value;
+		}
+	}
 
-    public XboxController(int port) {
-        xboxController = new Joystick(port);
-    }
+	public XboxController(int port) {
+		xboxController = new Joystick(port);
+	}
 
-    public boolean getButton(XboxController.Button inputButton) {
-        return xboxController.getRawButton(inputButton.value);
-    }
+	public boolean getButton(XboxController.Button inputButton) {
+		return xboxController.getRawButton(inputButton.value);
+	}
 
-    public Joystick getJoystick() {
-        return xboxController;
-    }
+	public Joystick getJoystick() {
+		return xboxController;
+	}
 
-    public double getAxis(int stickNumber, int axisNumber) {
-        int axes[] = {2, 1, 5, 4};
-        int fAxis = 0;
-        if (stickNumber == 1 && axisNumber == 1) {
-            fAxis = axes[0];
-        } else if (stickNumber == 1 && axisNumber == 2) {
-            fAxis = axes[1];
-        } else if (stickNumber == 2 && axisNumber == 1) {
-            fAxis = axes[2];
-        } else if (stickNumber == 2 && axisNumber == 2) {
-            fAxis = axes[3];
-        } else {
-            return -74;
-        }
-        return xboxController.getRawAxis(fAxis);
-    }
-    
-    public double getTriggerAxis() {
-        return xboxController.getRawAxis(3);
-    }
+	public double getAxis(int stickNumber, int axisNumber) {
+		int axes[] = {2, 1, 5, 4};
+		int fAxis = 0;
+		if (stickNumber == 1 && axisNumber == 1) {
+			fAxis = axes[0];
+		} else if (stickNumber == 1 && axisNumber == 2) {
+			fAxis = axes[1];
+		} else if (stickNumber == 2 && axisNumber == 1) {
+			fAxis = axes[2];
+		} else if (stickNumber == 2 && axisNumber == 2) {
+			fAxis = axes[3];
+		} else {
+			return -74;
+		}
+		return xboxController.getRawAxis(fAxis);
+	}
+
+	public double getTriggerAxis() {
+		return xboxController.getRawAxis(3);
+	}
 }
