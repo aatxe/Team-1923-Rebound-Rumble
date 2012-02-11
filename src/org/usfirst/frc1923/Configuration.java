a1e78ccbc0a06c1a07032873ce09835a3bee3870
Aaron Weiss <aaronweiss74@gmail.com>
2012-02-11 11:43:27 -0500

    Added experimental drive option with new configuration option.

--- src/org/usfirst/frc1923/Configuration.java
+++ src/org/usfirst/frc1923/Configuration.java
@@ -3,6 +3,7 @@ package org.usfirst.frc1923;
 public class Configuration {
 	public static final boolean reversedControls = false;
 	public static final boolean gearShifter = true;
+	public static final boolean experimentalDriving = false;
 	/* 0 1 2 3 4 5 6 7 8 9 10 */
 	// public static final double[] driveGears = {0.50, 0.55, 0.60, 0.65, 0.70,
 	// 0.75, 0.80, 0.85, 0.90, 0.95, 1.00};
